77272d77429e4eb877807c0652dc5cab62fdc9dc
Chiba Takaki <takaki200109@gmail.com>
2021-10-06 11:25:33 +0900

    kadai13

--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -1,4 +1,19 @@
-<!DOCTYPE html>
+@extends('layouts.profile')
+
+@section('title', 'My Edit')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>My Edit</h2>
+            </div>
+        </div>
+    </div>
+@endsection
+
+
+<!-- <!DOCTYPE html>
 <html>
   <head>
     <meta charset="utf-8">
@@ -10,4 +25,4 @@
   <body>
     <h1>Myプロフィール編集画面</h1>
   </body>
-</html>
+</html> -->
