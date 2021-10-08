716270cc3c934b6794b700af1abd5c2b6d9efe89
Chiba Takaki <takaki200109@gmail.com>
2021-10-08 12:49:46 +0900

    kadai14

--- routes/web.php
+++ routes/web.php
@@ -18,11 +18,13 @@ Route::get('/', function () {
 //   Route::get('XXX', 'AAAController@bbb');
 // });
 
-Route::group(['prefix' => 'admin'], function() {
-  Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
-  Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
-  Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
-
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
+  Route::get('news/create', 'Admin\NewsController@add');
+  Route::get('profile/create', 'Admin\ProfileController@add');
+  Route::get('profile/edit', 'Admin\ProfileController@edit');
+  Route::post('news/create', 'Admin\NewsController@create');
+  Route::post('profile/create', 'Admin\ProfileController@create');
+  Route::post('profile/edit', 'Admin\ProfileController@update');
 });
 
 Auth::routes();
