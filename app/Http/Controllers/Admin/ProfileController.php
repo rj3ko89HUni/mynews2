cddf89da2d31583e479b69d4512c043b04f32586
Chiba Takaki <takaki200109@gmail.com>
2021-10-13 11:35:45 +0900

    kadai17

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -19,6 +19,7 @@ class ProfileController extends Controller
       $this->validate($request, Profile::$rules);
 
       $profile = new Profile;
+
       $form = $request->all();
 
       // フォームから送信されてきた_tokenを削除する
@@ -31,14 +32,50 @@ class ProfileController extends Controller
       return redirect('admin/profile/create');
   }
 
-  public function edit()
+  public function index(Request $request)
   {
-      return view('admin.profile.edit');
+      $cond_name = $request->cond_name;
+      if ($cond_name != '') {
+          // 検索されたら検索結果を取得する
+          $posts = Profile::where('name', $cond_name)->get();
+      } else {
+          // それ以外はすべてのニュースを取得する
+          $posts = Profile::all();
+      }
+      return view('admin.profile.index', ['posts' => $posts, 'cond_name' => $cond_name]);
   }
 
-  public function update()
+  public function edit(Request $request)
   {
-      return redirect('admin/profile/edit');
+    $profile = Profile::find($request->id);
+    if (empty($profile)) {
+      abort(404);
+    }
+    return view('admin.profile.edit', ['profile_form' => $profile]);
+  }
+
+  public function update(Request $request)
+  {
+    // Validationをかける
+    $this->validate($request, Profile::$rules);
+    // News Modelからデータを取得する
+    $profile = Profile::find($request->id);
+    // 送信されてきたフォームデータを格納する
+    $profile_form = $request->all();
+
+    unset($profile_form['_token']);
+    // 該当するデータを上書きして保存する
+    $profile->fill($profile_form)->save();
+    return redirect('admin/profile');
+  }
+
+  public function delete(Request $request)
+  {
+    // 該当するNews Modelを取得
+    $profile = Profile::find($request->id);
+    // 削除する
+    $profile->delete();
+    return redirect('admin/profile/');
   }
 
 }
--- routes/web.php
+++ routes/web.php
@@ -20,9 +20,15 @@ Route::get('/', function () {
 
 Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
   Route::get('news/create', 'Admin\NewsController@add');
+  Route::get('news/edit', 'Admin\NewsController@edit');
+  Route::get('news', 'Admin\NewsController@index');
+  Route::get('news/delete', 'Admin\NewsController@delete');
   Route::get('profile/create', 'Admin\ProfileController@add');
   Route::get('profile/edit', 'Admin\ProfileController@edit');
+  Route::get('profile', 'Admin\ProfileController@index');
+  Route::get('profile/delete', 'Admin\ProfileController@delete');
   Route::post('news/create', 'Admin\NewsController@create');
+  Route::post('news/edit', 'Admin\NewsController@update');
   Route::post('profile/create', 'Admin\ProfileController@create');
   Route::post('profile/edit', 'Admin\ProfileController@update');
 });
